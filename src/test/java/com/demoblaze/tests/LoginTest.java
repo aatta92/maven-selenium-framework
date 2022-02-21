e86a8d9ac0a33388081ec02450e8621b8528c418
ilzat <cyramsolutions@gmail.com>
2022-02-21 15:49:06 -0500

    fixed null driver

--- src/test/java/com/demoblaze/tests/LoginTest.java
+++ src/test/java/com/demoblaze/tests/LoginTest.java
@@ -5,12 +5,13 @@ import com.demoblaze.utils.Library;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
+
 public class LoginTest extends Base {
 
-    Library lib = new Library(driver);
 
     @Test
     public void loginWithValidAccount_Test() throws InterruptedException {
+        Library lib = new Library(driver);
         lib.click(By.cssSelector("#login2"));
         Thread.sleep(1000);
         login("geek", "geek");
@@ -18,12 +19,14 @@ public class LoginTest extends Base {
 
     @Test
     public void loginWithInvalidAccount_Test() throws InterruptedException {
+        Library lib = new Library(driver);
         lib.click(By.cssSelector("#login2"));
         Thread.sleep(1000);
         login("hsgdhgfhgdsf", "dhsfhdsgfd");
     }
 
     private void login(String username, String password) {
+        Library lib = new Library(driver);
         lib.enterText(By.cssSelector("#loginusername"), username);
         lib.enterText(By.cssSelector("#loginpassword"), password);
         lib.click(By.cssSelector("[onclick='logIn()']"));
