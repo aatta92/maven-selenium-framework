efd9f75b0141af4111db112d8da161cdb8c5c093
ilzat <cyramsolutions@gmail.com>
2022-02-22 10:36:28 -0500

    Logout test

---
+++ src/test/java/com/demoblaze/tests/smoketests/LogoutTest.java
@@ -0,0 +1,15 @@
+package com.demoblaze.tests.smoketests;
+
+import com.demoblaze.base.Base;
+
+public class LogoutTest extends Base {
+
+    /**
+     * Test case: Log out
+     * Test steps:
+     * 1) User is logged in
+     * 2) Click the logout link on the header
+     * 3) Verify that user is logged out successfully
+     */
+
+}
